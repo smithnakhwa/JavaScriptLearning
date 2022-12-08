787ca70730c27ae3d522e78487af1c66194a52a3
smithnakhwa <nakhwasmith@gmail.com>
2022-12-08 08:56:56 +0530

    Js code
    js code and assignment

---
+++ 01_variable_assign.js
@@ -0,0 +1,23 @@
+var myFullName;
+console.log(myFullName);
+myFullName="Smith Nakhwa";
+console.log(myFullName);
+myFullName="Smith sachin nakhwa";
+console.log(myFullName);
+var myLove="i love only JS";
+console.log(myLove);
+var firstName="Smith";
+var lastName="Nakhwa";
+var age="22";
+var address="R/o:-Karanja Navapada,Uran";
+var pincode="400-702";
+var gender="Male";
+var is_married="No";
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(address);
+console.log(pincode);
+console.log(gender);
+console.log(is_married);
+
---
+++ hello.js
@@ -0,0 +1,7 @@
+var myName;// Variable Declaration
+myName="Smith nakhwa";//variable initialization
+console.log(myName);
+var myFrdName="god father";//Variable Declaration and  initialization at same time
+console.log(myFrdName);
+myFrdName="Thomas shelby";// Variable update
+console.log(myFrdName);

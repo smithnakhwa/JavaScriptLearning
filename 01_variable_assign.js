8a83f5bc836899e55f24b17fdeedea1f3bcf9fa3
smithnakhwa <nakhwasmith@gmail.com>
2022-12-09 19:39:40 +0530

    swap value
    swap value

--- 01_variable_assign.js
+++ 01_variable_assign.js
@@ -12,12 +12,15 @@ var age="22";
 var address="Address:-R/o:-Karanja Navapada,Uran";
 var pincode="pincode:-400-702";
 var gender="gender:-Male";
-var is_married="is_married:-No";
+var is_Married="is_married:-No";
 console.log(firstName);
 console.log(lastName);
 console.log(age);
 console.log(address);
 console.log(pincode);
 console.log(gender);
-console.log(is_married);
+console.log(is_Married);
+var name =true;
+console.log(typeof name );
+console.log(typeof name );
 
